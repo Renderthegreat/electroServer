a0082b4cba3d205ce65b00471b37135a480cf832
renderthegreat <renderthegreat.business@gmail.com>
2024-03-07 08:45:51 +0000

    Fix it

---
+++ nuxt-edge/node_modules/db0/connectors/cloudflare-d1.cjs
@@ -0,0 +1,42 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+module.exports = sqliteConnector;
+function sqliteConnector(options) {
+  const getDB = () => globalThis.__cf_env__[options.bindingName];
+  return {
+    name: "cloudflare-d1",
+    exec: sql => getDB().exec(sql),
+    prepare: sql => {
+      const _stmt = getDB().prepare(sql);
+      const onError = err => {
+        if (err.cause) {
+          err.message = err.cause.message + ' "' + sql + '"';
+        }
+        throw err;
+      };
+      const stmt = {
+        bind(...params) {
+          _stmt.bind(...params);
+          return stmt;
+        },
+        all(...params) {
+          return _stmt.bind(...params).all().catch(onError);
+        },
+        run(...params) {
+          return _stmt.bind(...params).run().then(res => {
+            return {
+              success: res.success
+            };
+          }).catch(onError);
+        },
+        get(...params) {
+          return _stmt.bind(...params).first().catch(onError);
+        }
+      };
+      return stmt;
+    }
+  };
+}
