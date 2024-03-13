7bfb126b39603010bc209982589f229b9498900f
renderthegreat <renderthegreat.business@gmail.com>
2024-03-13 12:27:26 +0000

    Stuff

--- host.composable.js
+++ host.composable.js
@@ -1,11 +1,12 @@
 module.exports = async function(Host, Server, Content, runtime, SSR){
-   let host = new Host();//creates a new host.
-   host.hostDir("get", "server", "/");  //this part host the server dir on path /.
+   const config = require("./ELECTRO.config.json");
+   let host = new Host();
+   host.hostDir("get", "server", "/");
    host.hostFile("get", "./readme.md", "/read");
    host.hostAPI("get", "./server/app.jsg", "/api")
    await host.nuxt("./nuxt-edge/api/edge.js", "/nuxt", "/")
-   await Server.start(80);//starts the server.
-   await runtime.sleep(250);//sleeps for 0.25 seconds.
+   await Server.start(config.port);
+   await runtime.sleep(250);
    runtime.log("Press (CTRL + Q) to pause. Or press (CTRL + E) to end.", "");
     
 }
